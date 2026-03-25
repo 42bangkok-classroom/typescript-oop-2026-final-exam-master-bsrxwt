6432fcf674f03d4c9487e9ebf72d48e34da27681
Sorawit Wongthim <68011120@kmitl.ac.th>
2026-03-25 10:37:08 +0700

    test

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -1,12 +1,12 @@
-import { Controller,Get, } from '@nestjs/common';
-  import { ProductService } from './product.service';
-  import { Product } from './product.interface';
-  @Controller('products')
-  export class ProductController {
-    constructor(private readonly productService: ProductService) {}
-    @Get()
-    findAll(): Product[] {
-      return this.productService.findAll();
-    }
- 
+import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { Product } from './product.interface';
+
+@Controller('products')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+  @Get()
+  findAll(): Product[] {
+    return this.productService.findAll();
   }
+}
