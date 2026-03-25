27fd0f91c6443844e0c045d7cd22e4ce4f4dcbbc
Sorawit Wongthim <68011120@kmitl.ac.th>
2026-03-25 10:28:40 +0700

    01

---
+++ src/products/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from './product.interface';
+import * as fs from 'fs';
+import * as path from 'path';
+
+@Injectable()
+export class ProductService {
+  findAll(): Product[] {
+    const fileData = fs.readFileSync(
+      path.join(process.cwd(), 'data', 'products.json'),
+      'utf-8',
+    );
+    return JSON.parse(fileData) as Product[];
+  }
+}
